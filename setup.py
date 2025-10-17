cbbe26e59032419e835c364d9e7d223ad5d4fb51
Anshul Kanakia <anshul.p.kanakia@gmail.com>
2025-10-17 11:12:58 -0700

    revert: Switching back to comminicate from wait.

--- setup.py
+++ setup.py
@@ -21,11 +21,11 @@ class CustomInstallCommand(install):
        print('INFO: Downloading germlines from IMGT and building HMMs...')
        print("INFO: running 'RUN_pipeline.sh', this will take a couple a minutes.")
        proc = subprocess.Popen(["bash", "RUN_pipeline.sh"], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-       proc.wait()  # This blocks until the process completes
+    #    proc.wait()  # This blocks until the process completes
 
-    #    o, e = proc.communicate()
-    #    print(o.decode())
-    #    print(e.decode())
+       o, e = proc.communicate()
+       print(o.decode())
+       print(e.decode())
        
        print("INFO: HMMs built. Installing data files...")
        # Copy HMMs where ANARCI can find them
